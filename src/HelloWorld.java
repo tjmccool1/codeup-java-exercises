844fcb7e3f39c73fc857f7e3b8827ab3038e15d7
TMcCool <tjmccool1@icloud.com>
2018-02-27 12:16:02 -0600

    Syntax, Types, and Variables Exercise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,57 @@ public class HelloWorld {
 //        System.out.println("Hello, World!");
         System.out.print("Hello, ");
         System.out.print("World! \n");
+
+        int myFavoriteNumber;
+        myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+
+        String myString;
+        myString = "My favorite number is ";
+        System.out.println(myString);
+
+        //myString = 'c';
+        //myString = 3.1459;
+
+        long myNumber;
+       // myNumber = 3.14;
+        myNumber = 123L;
+        myNumber = 123;
+        //float myNumber = myNumber;
+        //float myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+/* The code above prints x then prints to new value of x
+* the code below prints the value of x + 1 then the new vale of x.
+ *
+ * */
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        //class is a reserve word.
+//        String class;
+//        class = "Teddy";
+//        System.out.println(class);
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+//
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
     }
 }
