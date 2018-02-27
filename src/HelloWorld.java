6d74b22ea7120b13815a1fd457f4b786d6c523fb
TMcCool <tjmccool1@icloud.com>
2018-02-27 11:08:41 -0600

    HelloWorld complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
 //        System.out.println("Hello, World!");
         System.out.print("Hello, ");
-        System.out.print("World!");
+        System.out.print("World! \n");
     }
 }
